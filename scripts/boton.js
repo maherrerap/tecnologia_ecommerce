76328e0b6dafaa2294f2143acde92e60e05ddb35
maria <mariapau142005@gmail.com>
2025-10-31 10:54:58 -0500

    fix: iphone.html - agrecar productos dinámicos

---
+++ scripts/boton.js
@@ -0,0 +1,10 @@
+const  locos = document.getElementById("btn_action");
+
+/*
+botones_locos.addEventListener("click", function() {
+    alert("hice click");
+})
+*/
+locos.addEventListener("click", function(event) {
+    console.log(event);
+})
